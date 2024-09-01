3af984d18cd7f82f17176ac9bc9a549aa96f4cc6
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-09-01 10:00:39 +0700

    design footer

--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -1,30 +1,36 @@
 
 'use client'
 import React from 'react';
-import { DownOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Dropdown, Space } from 'antd';
-const items: MenuProps['items'] = [
-    {
-        label: <a href="https://www.antgroup.com">1st menu item</a>,
-        key: '0',
-    },
-    {
-        label: <a href="https://www.aliyun.com">2nd menu item</a>,
-        key: '1',
-    },
-    {
-        type: 'divider',
-    },
-    {
-        label: '3rd menu item',
-        key: '3',
-    },
-];
+import { FacebookOutlined, InstagramOutlined, YoutubeOutlined } from '@ant-design/icons';
+import 'src/styles/footer/app.footer.scss';
+import Link from 'next/link';
+
 const AppFooter = () => {
     return (
         <>
-            Footer
+            <div className='footer'>
+                <div className='footer__nav'>
+                    <Link href={"/"}>Chính sách</Link>
+                    <Link href={"/"}>Lịch chiếu</Link>
+                    <Link href={"/"}>Tin tức</Link>
+                    <Link href={"/"}>Giá vé</Link>
+                    <Link href={"/"}>Hỏi đáp</Link>
+                    <Link href={"/"}>Liên hệ</Link>
+                </div>
+                <div className='footer__icon'>
+                    <div className='facebook'><FacebookOutlined /></div>
+                    <div className='youtube'><YoutubeOutlined /></div>
+                    <div className='instagram'><InstagramOutlined /></div>
+                    <div className='googleplay'><img src='home/googleplay.png' /></div>
+                    <div className='appstore'><img src='home/appstore.png' /></div>
+                    <div className='certification'><img src='home/certification.png' /></div>
+
+                </div>
+                <div className='footer__text'>
+                    Copyright 2023. NCC All Rights Reservered. Dev by
+                    <Link href={"/https://www.facebook.com/profile.php?id=100079731466553"}>Quang Duy</Link>
+                </div>
+            </div>
         </>
     )
 }
