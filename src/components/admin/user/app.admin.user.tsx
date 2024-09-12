3d8edebe1610874a7ea9f06b98996c94bdd99d93
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-09-12 15:04:24 +0700

    fetch data for page

--- src/components/admin/user/app.admin.user.tsx
+++ src/components/admin/user/app.admin.user.tsx
@@ -1,62 +1,183 @@
 "use client"
 
-import { Table } from "antd";
+import { Button, ConfigProvider, Popover, Table } from "antd";
 import { useEffect, useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { LuPenLine } from "react-icons/lu";
+import { MdDelete, MdInfoOutline } from "react-icons/md";
+import { callFetchAllUsers } from "src/util/api";
+import 'src/styles/admin/user/app.admin.user.scss'
 
 const AppAdminUser = () => {
 
     // STATE: 
-    const [data, setData] = useState<IModelPaginate<IUser>>();
+    const [data, setData] = useState<IUser[]>();
     const [loading, setLoading] = useState<boolean>(false);
-    const [current, setCurrent] = useState<number>(1);
-    const [pageSize, setPageSize] = useState<number>(2);
+    const [page, setPage] = useState<number>(1);
+    const [size, setSize] = useState<number>(2);
     const [total, setTotal] = useState<number>(0);
     const [isSearch, setIsSearch] = useState<boolean>(false);
 
 
     // EFFECT:
     useEffect(() => {
+        fetchUser();
+        console.log(">> check data: ", data)
+    }, [page, size]);
 
-    }, [])
-
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
+    const titleDelete = (
+        <>
+            <span className='del-title'><MdInfoOutline className='del-icon' />
+                <> </> Xác nhận xóa
+            </span>
+        </>
+    )
 
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
+            title: 'ID',
+            dataIndex: 'id',
+            width: "5%",
+            key: 'id',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Tên hiển thị',
+            dataIndex: 'fullName',
+            sorter: true,
+            width: "10%",
+            key: "fullName"
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            sorter: true,
+            width: "20%",
+            key: "fullName"
+
+        },
+        {
+            title: 'Phone',
+            dataIndex: 'phone',
+            sorter: true,
+            width: "20%",
+            key: "phone"
         },
         {
             title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
+            dataIndex: "address",
+            sorter: true,
+            width: "20%",
+            key: "address",
         },
+
+        {
+            title: 'Action',
+            width: "25%",
+            key: "action",
+            render: (text: string, record: any, index: number) => {
+                return (
+                    <>
+                        <div className='action'>
+                            <ConfigProvider>
+                                <Popover
+                                    placement="rightTop"
+                                    title={titleDelete}
+                                    trigger={"click"}
+                                    content={
+                                        <div className='del-content'>
+                                            <Button
+                                                type='primary'
+                                                className='confirm'
+                                            // onClick={() => handleDelete(record._id)}
+                                            >Xác nhận</Button>
+                                        </div>
+                                    }
+                                >
+                                    <span className='del-btn'>
+                                        <MdDelete />
+                                    </span>
+                                </Popover>
+                            </ConfigProvider>
+                            <span className='view-btn'
+                            // onClick={() => handleOnView(record)}
+                            >
+                                <FaEye />
+                            </span>
+                            <span
+                                className='update-btn'
+                            // onClick={() => handleUpdate(record)}
+                            >
+                                <LuPenLine />
+                            </span>
+                        </div>
+                    </>
+                )
+            }
+        }
+
     ];
+
+    const fetchUser = async () => {
+        setLoading(true);
+        let query = `?page=${page}&size=${size}`;
+
+        // if (querySearch) {
+        //     query += querySearch;
+        // }
+
+        // if (sortQuery) {
+        //     query += sortQuery;
+        // }
+
+
+        const res = await callFetchAllUsers(query);
+        console.log(">> check res: ", res)
+        if (res && res?.data) {
+            setData(res.data.result);
+            setLoading(false);
+            setTotal(res.data.meta.total);
+        }
+    };
+
+    const handleTableChange = (pagination: { size: number, page: number }, filters: any, sorter: any, extra: any) => {
+        if (pagination && pagination.page !== page)
+            setPage(pagination.page);
+        if (pagination && pagination.size !== size) {
+            setPage(1);
+            setSize(pagination.size);
+        }
+
+        // `dataSource` is useless since `pageSize` changed
+        if (pagination.size !== size) {
+            setData([]);
+        }
+
+        // if (sorter && sorter.field) {
+        //     if (sorter.order == 'ascend') {
+        //         setSortQuery(`&sort=${sorter.field}`)
+        //     }
+
+        //     if (sorter.order == 'descend') {
+        //         setSortQuery(`&sort=-${sorter.field}`)
+        //     }
+        // }
+    };
 
     return (
         <>
             <Table
-                dataSource={dataSource}
+                dataSource={data}
                 columns={columns}
+                pagination={{
+                    current: page,
+                    pageSize: size,
+                    showSizeChanger: true,
+                    total: total,
+                    showTotal: (total, range) =>
+                        <div>{range[0]} - {range[1]} trên {total} dòng</div>
+                }}
+                loading={loading}
+                onChange={handleTableChange}
             />
         </>
     )
